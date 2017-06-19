ddeec51eb46db54fe03a6f22cd9bbbbb0d4a248f
norbyariel02 <norbertowinkler@gmail.com>
2017-06-19 20:45:01 -0300

    player inventory

--- MiPrimerProyecto2D/Assets/Scripts/Player/PlayerInventory.cs
+++ MiPrimerProyecto2D/Assets/Scripts/Player/PlayerInventory.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 public class PlayerInventory : MonoBehaviour {
 	public bool HasKey = false;
 	// Use this for initialization
+
+	//private varible
+	private Dictionary<string, int> Inventory; 
+
+	void Awake()
+	{
+		Inventory =  new Dictionary<string, int>();
+		Inventory.Add("key", 0);
+		Inventory.Add("Weapon02", 0);
+	}
+
 	void Start () {
 		
 	}
@@ -20,6 +31,22 @@ public class PlayerInventory : MonoBehaviour {
 			Destroy (other.gameObject);
 			HasKey = true;
 		}
+	}
+
+	public void SetElement(string nameKey, int value)
+	{
+		if (Inventory.ContainsKey(nameKey))
+			Inventory[nameKey] += value;
+		else
+			Inventory.Add(nameKey, value);
+	}
+
+	public int GetElemnt(string nameKey)
+	{
+		if (!Inventory.ContainsKey(nameKey))
+			return 0;
+
+		return Inventory[nameKey];
 	}
 
 }
